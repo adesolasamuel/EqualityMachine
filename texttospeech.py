23ae93b92e0484e2b88fcbd31ac1b4f3552cfa66
Samuel Adesola <55460620+adesolasamuel@users.noreply.github.com>
2022-06-26 01:58:01 +0100

    Update texttospeech.py

--- texttospeech.py
+++ texttospeech.py
@@ -4,11 +4,11 @@ import os
 
 #MALE
 engine = pyttsx3.init()
-engine.say("Hello there, cannot open shared object file: No such file or directory")
+engine.say("Hello there")
 engine.runAndWait()
 
 #FEMALE
-mytext = 'You are welcome to Roles Academy Madam. About Company:Roles Academy is a leading institute in Delhi NCR offering computer education courses and foreign languages like Spanish, German, French, etc. Roles Academy also provides web design courses in Delhi, including mobile web design course and content writing courses in Delhi. For more details please call +91-9971110009 or visit the website: www.rolesacademy.com.Roles Academy is a leading institute'
+mytext = 'You are welcome to Roles Academy Madam.'
 language = 'en'
 myobj = gTTS(text=mytext, lang=language, slow=False)
 myobj.save("welcome.mp3")
